f7d66c9bfa2313d2387e65d2fe4d1757b2abac36
Luis Guisado <lguisadom@gmail.com>
2022-07-23 00:52:31 -0500

    The users microservice project is added as a child project

---
+++ msvc-users/src/test/java/com/lagm/springcloud/msvc/users/MsvcUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lagm.springcloud.msvc.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
